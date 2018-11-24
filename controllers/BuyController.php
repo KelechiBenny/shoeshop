bc98b99d4267c433f2daefa93a788f51ceb172f9
Kelechi <chihurumnannya@gmail.com>
2018-11-24 16:08:54 +0100

    reverted back

--- controllers/BuyController.php
+++ controllers/BuyController.php
@@ -1,11 +1,14 @@
 <?php
 namespace Controllers;
 
+use Models\Buy;
+
 class BuyController
 {
 
     public function view()
     {
-        
+        // die('here');
+        (new Buy)->view();
     }
 }
