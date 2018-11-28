f6491fb899bf0f8a84cbd823d02dc1229eeea20e
Ichoku Great <greatchinonso7@gmail.com>
2018-11-28 07:59:57 -0800

    Updated the login Module

--- models/Login.php
+++ models/Login.php
@@ -3,12 +3,27 @@ namespace Models;
 
 class Login extends Connection
 {
-    public $email;
-    public $fullname;
-    public $password;
-    public $phone;
+   public function __construct()
+   {
+       $this->conn = $this->getConnection();
+   }
 
-    public function login() {
-        
-    }
+   public function login($email, $password)
+   {
+        $sql = "SELECT * FROM users WHERE `email` = '$email' and `password` = '$password'";
+
+        $result = $this->conn->query($sql);
+
+        if (!$result) {
+            die('Could not login '. $this->conn->error);
+        }else {
+            if ($result->num_rows == 1) {
+                $_SESSION['user'] = $email;
+                echo ('<script>location.href= "index.php";</script>');
+            }else {
+                echo '<p class="error"> Incorrect Email or Password</p>';
+            }
+        }
+   }
+
 }
